812049c71644c9c3d0541d7b38ce58fce0fa365a
june63 <lea57.kieffer@gmail.com>
2021-12-23 08:53:51 +0100

    "model"

---
+++ Reciplease/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Reciplease
+//
+//  Created by Léa Kieffer on 23/12/2021.
+//
+
+import Foundation
+
+// MARK: - Constants project
+
+enum Constants {
+    enum Storyboard {
+        static let main = "Main"
+        static let listViewController = "ListViewController"
+        static let detailView = "DetailViewController"
+        static let recipeCellId = "recipeCellId"
+        static let ingredientsCellId = "ingredientsCell"
+    }
+}
+
